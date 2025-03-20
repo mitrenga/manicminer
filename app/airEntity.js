12b9fccfc5e96a5d917f10621e13e96fe8b79540
Libor Mitrenga <libor@mitrengovi.cz>
2025-03-20 19:14:27 +0100

    Oprava AirEntity

--- app/airEntity.js
+++ app/airEntity.js
@@ -8,17 +8,19 @@ import AbstractEntity from './svision/js/abstractEntity.js';
 export class AirEntity extends AbstractEntity {
 
   constructor(parentEntity, x, y, width, height) {
-    super(parentEntity, x, y, width, height);
+    super(parentEntity, x, y, width, height, false, false);
     this.id = 'AirEntity';
+    
+    this.bkColor = this.app.platform.colorByName('brightWhite');
   } // constructor
 
   drawEntity() {
     super.drawEntity();
 
-    this.app.layout.paint(0, 0, 6*8, 2, this.app.platform.colorByName('brightRed'));
-    this.app.layout.paint(0, 6, 6*8, 2, this.app.platform.colorByName('brightRed'));
-    this.app.layout.paint(6*8, 0, this.width-6*8, 2, this.app.platform.colorByName('brightGreen'));
-    this.app.layout.paint(6*8, 6, this.width-6*8, 2, this.app.platform.colorByName('brightGreen'));
+    this.app.layout.paint(this, 0, 0, 6*8, 2, this.app.platform.colorByName('brightRed'));
+    this.app.layout.paint(this, 0, 6, 6*8, 2, this.app.platform.colorByName('brightRed'));
+    this.app.layout.paint(this, 6*8, 0, this.width-6*8, 2, this.app.platform.colorByName('brightGreen'));
+    this.app.layout.paint(this, 6*8, 6, this.width-6*8, 2, this.app.platform.colorByName('brightGreen'));
   } // drawEntity
 
 } // class AirEntity
