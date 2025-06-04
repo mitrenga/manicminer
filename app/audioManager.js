8ce697290994361d6e198af8992c4b2454a4fd8b
Libor Mitrenga <libor@mitrengovi.cz>
2025-06-04 09:59:09 +0200

    Zjednodušení kódu pro createAudioHandler

--- app/audioManager.js
+++ app/audioManager.js
@@ -1,9 +1,11 @@
 /**/
 const { AbstractAudioManager } = await import('./svision/js/abstractAudioManager.js?ver='+window.srcVersion);
-const { appAudioHandler } = await import('./appAudioHandler.js?ver='+window.srcVersion);
+const { AudioWorkletHandler } = await import('./svision/js/audioWorkletHandler.js?ver='+window.srcVersion);
+const { AudioScriptProcessorHandler } = await import('./svision/js/audioScriptProcessorHandler.js?ver='+window.srcVersion);
 /*/
 import AbstractAudioManager from './svision/js/abstractAudioManager.js';
-import appAudioHandler from './appAudioHandler.js';
+import AudioWorkletHandler from './svision/js/audioWorkletHandler.js';
+import AudioScriptProcessorHandler from './svision/js/audioScriptProcessorHandler.js';
 /**/
 // begin code
 
@@ -17,7 +19,18 @@ export class AudioManager extends AbstractAudioManager {
   } // constructor
 
   createAudioHandler() {
-    return appAudioHandler(this.app);
+    var audioHandler = false;
+
+    switch (this.unsupportedAudioChannel) {
+      case false:
+        audioHandler = new AudioWorkletHandler(this.app);
+        break;
+      case 'AudioWorkletHandler':
+        audioHandler = new AudioScriptProcessorHandler(this.app);
+        break;
+    }
+
+    return audioHandler;
   } // createAudioHandler
 
 } // class AudioManager
