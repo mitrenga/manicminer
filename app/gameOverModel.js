7359040187ba2fdd6e47a9b0e5e10ebd316bcf6a
Libor Mitrenga <libor@mitrengovi.cz>
2025-08-30 19:41:12 +0200

    GameOverModel umí pokračování ve hře

--- app/gameOverModel.js
+++ app/gameOverModel.js
@@ -4,12 +4,14 @@ const { AbstractEntity } = await import('./svision/js/abstractEntity.js?ver='+wi
 const { GameInfoEntity } = await import('./gameInfoEntity.js?ver='+window.srcVersion);
 const { SpriteEntity } = await import('./svision/js/platform/canvas2D/spriteEntity.js?ver='+window.srcVersion);
 const { PillarEntity } = await import('./pillarEntity.js?ver='+window.srcVersion);
+const { ZXTextEntity } = await import('./svision/js/platform/canvas2D/zxSpectrum/zxTextEntity.js?ver='+window.srcVersion);
 /*/
 import AbstractModel from './svision/js/abstractModel.js';
 import AbstractEntity from './svision/js/abstractEntity.js';
 import GameInfoEntity from './gameInfoEntity.js';
 import SpriteEntity from './svision/js/platform/canvas2D/spriteEntity.js';
 import PillarEntity from './pillarEntity.js';
+import ZXTextEntity from './svision/js/platform/canvas2D/zxSpectrum/zxTextEntity.js';
 /**/
 // begin code
 
@@ -22,6 +24,12 @@ export class GameOverModel extends AbstractModel {
     this.gameInfoEntity = null;
     this.pillarEntity = null;
     this.shoeEntity = null;
+    this.gameEntity = null;
+    this.overEntity = null;
+    this.contiueEntity = null;
+    this.timerEntity = null;
+    this.colorCounter = 65;
+    this.colorTimer = false;
   } // constructor
 
   init() {
@@ -47,23 +55,110 @@ export class GameOverModel extends AbstractModel {
     shoeSpriteEntity.setGraphicsData(this.app.globalData.gameOver.shoe);
     this.pillarEntity = new PillarEntity(this.desktopEntity, 15*8, 0, 16, 0, this.app.platform.penColorByAttr(this.app.hexToInt(this.app.globalData.gameOver.pillar.attribute)), this.app.globalData.gameOver.pillar);
     this.desktopEntity.addEntity(this.pillarEntity);
+
+    this.gameEntity = new ZXTextEntity(this.desktopEntity, 10*8, 6*8, 4*8, 8, 'Game', this.app.platform.colorByName('brightWhite'), false, 0, true);
+    this.gameEntity.hide = true;
+    this.gameEntity.penColorsMap = {};
+    for (var ch = 0; ch < 4; ch++) {
+      this.gameEntity.penColorsMap[ch] = this.app.platform.penColorByAttr(this.colorCounter);
+      this.colorCounter = this.app.rotateInc(this.colorCounter, 65, 71);
+    }
+    this.desktopEntity.addEntity(this.gameEntity);
+    this.overEntity = new ZXTextEntity(this.desktopEntity, 18*8, 6*8, 4*8, 8, 'Over', this.app.platform.colorByName('brightWhite'), false, 0, true);
+    this.overEntity.hide = true;
+    this.overEntity.penColorsMap = {};
+    for (var ch = 0; ch < 4; ch++) {
+      this.overEntity.penColorsMap[ch] = this.app.platform.penColorByAttr(this.colorCounter);
+      this.colorCounter = this.app.rotateInc(this.colorCounter, 65, 71);
+    }
+    this.desktopEntity.addEntity(this.overEntity);
+    this.contiueEntity = new ZXTextEntity(this.desktopEntity, 0, 21*8, 32*8, 8, 'Press ENTER to continue', this.app.platform.colorByName('brightWhite'), false, 0, true);
+    this.contiueEntity.hide = true;
+    this.contiueEntity.justify = 2;
+    this.desktopEntity.addEntity(this.contiueEntity);
+    this.timerEntity = new ZXTextEntity(this.desktopEntity, 0, 23*8, 32*8, 8, '10', this.app.platform.colorByName('brightWhite'), false, 0, true);
+    this.timerEntity.hide = true;
+    this.timerEntity.justify = 2;
+    this.desktopEntity.addEntity(this.timerEntity);
   } // init
+
+  handleEvent(event) {
+    if (super.handleEvent(event)) {
+      return true;
+    }
+
+    switch (event.id) {
+      case 'keyPress':
+        switch (event.key) {
+          case 'Enter':
+            this.app.model.shutdown();
+            this.app.score = 0;
+            this.app.lastBonusScore = 0;
+            this.app.lives = 2;
+            this.app.demo = false;
+            this.app.model = this.app.newModel('CaveModel');
+            this.app.model.init();
+            this.app.resizeApp();
+            return true;
+          case 'Escape':
+            this.app.model.shutdown();
+            this.app.model = this.app.newModel('MainModel');
+            this.app.model.init();
+            this.app.resizeApp();
+            return true;
+        }
+        break;
+      case 'MainModel':
+        this.app.model.shutdown();
+        this.app.model = this.app.newModel('MainModel');
+        this.app.model.init();
+        this.app.resizeApp();
+        return true;
+    }
+    return false;
+  } // handleEvent
 
   loopModel(timestamp) {
     super.loopModel(timestamp);
 
     if (this.timer === false) {
       this.timer = timestamp;
-    } else {
-      var fallTimer = timestamp-this.timer;
-      if (fallTimer > 2000) {
-        fallTimer = 2000;
+    }
+    var fallTimer = timestamp-this.timer;
+    if (fallTimer > 2000) {
+      fallTimer = 2000;
+      this.gameEntity.hide = false;
+      this.overEntity.hide = false;
+      this.contiueEntity.hide = false;
+      this.timerEntity.hide = false;
+      if (this.colorTimer === false) {
+        this.colorTimer = timestamp;
+      }
+      if (timestamp-this.colorTimer > 30) {
+        this.colorTimer = timestamp;
+        for (var ch = 0; ch < 4; ch++) {
+          this.gameEntity.penColorsMap[ch] = this.app.platform.penColorByAttr(this.colorCounter);
+          this.colorCounter = this.app.rotateInc(this.colorCounter, 65, 71);
+        }
+        this.gameEntity.drawingCache[0].cleanCache();
+        for (var ch = 0; ch < 4; ch++) {
+          this.overEntity.penColorsMap[ch] = this.app.platform.penColorByAttr(this.colorCounter);
+          this.colorCounter = this.app.rotateInc(this.colorCounter, 65, 71);
+        }
+        this.overEntity.drawingCache[0].cleanCache();
+      }
+      var countdown = 10-Math.floor((timestamp-this.timer-2000)/1000);
+      if (countdown < 0) {
+        this.sendEvent(0, {'id': 'MainModel'});
+      } else {
+        this.timerEntity.setText(countdown.toString());
       }
     }
     this.desktopEntity.bkColor = this.app.platform.color((Math.floor((fallTimer%200)/50)));
     this.shoeEntity.bkColor = this.desktopEntity.bkColor;
     this.shoeEntity.y = Math.round(12*8*fallTimer/2000);
     this.pillarEntity.height = this.shoeEntity.y;
+
     this.drawModel();
   } // loopModel
 
