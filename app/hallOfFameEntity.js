32e994e9f44d30f6f2d3743a1e75d500b0b32c39
Libor Mitrenga <libor@mitrengovi.cz>
2025-09-20 14:01:13 +0200

    Příprava HallOfFameEntity

--- app/hallOfFameEntity.js
+++ app/hallOfFameEntity.js
@@ -2,10 +2,12 @@
 const { AbstractEntity } = await import('./svision/js/abstractEntity.js?ver='+window.srcVersion);
 const { MiniTextEntity } = await import('./svision/js/platform/canvas2D/miniTextEntity.js?ver='+window.srcVersion);
 const { MiniButtonEntity } = await import('./svision/js/platform/canvas2D/miniButtonEntity.js?ver='+window.srcVersion);
+const { ZXTextEntity } = await import('./svision/js/platform/canvas2D/zxSpectrum/zxTextEntity.js?ver='+window.srcVersion);
 /*/
 import AbstractEntity from './svision/js//abstractEntity.js';
 import MiniTextEntity from './svision/js/platform/canvas2D/miniTextEntity.js';
 import MiniButtonEntity from './svision/js/platform/canvas2D/miniButtonEntity.js';
+import ZXTextEntity from './svision/js/platform/canvas2D/zxSpectrum/zxTextEntity.js';
 /**/
 // begin code
 
@@ -13,7 +15,20 @@ export class HallOfFameEntity extends AbstractEntity {
 
   constructor(parentEntity, x, y, width, height) {
     super(parentEntity, x, y, width, height, false, false);
-    this.id = 'HallOfFameEntity';    
+    this.id = 'HallOfFameEntity';
+
+    this.tableHallOfFame = {
+      0: {name: 'bibix', score: '121381'},
+      1: {name: 'Matthew Smith', score: '95020'},
+      2: {name: 'bibix', score: '80000'},
+      3: {name: 'Libor Mitrenga', score: '7052'},
+      4: {name: 'hunter 007', score: '6111'},
+      5: {name: 'narci', score: '5234'},
+      6: {name: 'perpe2a', score: '4325'},
+      7: {name: 'noname001', score: '3333'},
+      8: {name: 'bibix', score: '2111'},
+      9: {name: 'Libor Mitrenga', score: '1220'}
+    };
   } // constructor
 
   init() {
@@ -26,12 +41,25 @@ export class HallOfFameEntity extends AbstractEntity {
     var titleEntity = new MiniTextEntity(this, 0, 0, this.width, 9, 'HALL OF FAME', this.app.platform.colorByName('brightWhite'), this.app.platform.colorByName('brightBlack'), 1, 2);
     titleEntity.justify = 2;
     this.addEntity(titleEntity);
+    for (var i = 0; i < 10; i++) {
+      var y = 12+i*10;
+      var entity = new ZXTextEntity(this, 2, y, 18, 8, (i+1)+'.', this.app.platform.colorByName('black'), false, 0, 1);
+      entity.justify = 1;
+      this.addEntity(entity);
+
+      entity = new ZXTextEntity(this, 22, y, 100, 8, this.tableHallOfFame[i].name, this.app.platform.colorByName('black'), false, 0, 1);
+      this.addEntity(entity);
+
+      entity = new ZXTextEntity(this, this.width-78, y, 76, 8, this.tableHallOfFame[i].score, this.app.platform.colorByName('black'), false, 0, 0);
+      entity.justify = 1;
+      this.addEntity(entity);
+    }
     this.addEntity(new MiniButtonEntity(this, this.width-38, this.height-15, 36, 13, 'CLOSE', 'closeAbout', ['Enter', 'Escape', ' '], this.app.platform.colorByName('brightWhite'), this.app.platform.colorByName('brightBlue'), 1, 4));
   } // init
 
   handleEvent(event) {
     switch (event.id) {
-      case 'mouseClick':
+      case 'closeAbout':
         this.destroy();
         return true;
     }
