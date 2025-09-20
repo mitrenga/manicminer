9ef8b4c8b9ab36b0b5a498f0ce1e9e254c784571
Libor Mitrenga <libor@mitrengovi.cz>
2025-09-20 09:13:43 +0200

    Příprava HallOfFameEntity

--- app/hallOfFameEntity.js
+++ app/hallOfFameEntity.js
@@ -1,9 +1,11 @@
 /**/
 const { AbstractEntity } = await import('./svision/js/abstractEntity.js?ver='+window.srcVersion);
 const { MiniTextEntity } = await import('./svision/js/platform/canvas2D/miniTextEntity.js?ver='+window.srcVersion);
+const { MiniButtonEntity } = await import('./svision/js/platform/canvas2D/miniButtonEntity.js?ver='+window.srcVersion);
 /*/
 import AbstractEntity from './svision/js//abstractEntity.js';
 import MiniTextEntity from './svision/js/platform/canvas2D/miniTextEntity.js';
+import MiniButtonEntity from './svision/js/platform/canvas2D/miniButtonEntity.js';
 /**/
 // begin code
 
@@ -16,12 +18,15 @@ export class HallOfFameEntity extends AbstractEntity {
 
   init() {
     super.init();
-    this.addEntity(new AbstractEntity(this, 1, 7, this.width-2, this.height-8, false, this.app.platform.colorByName('brightWhite')));
+    this.addEntity(new AbstractEntity(this, 1, 7, this.width-2, this.height-8, false, this.app.platform.colorByName('yellow')));
     this.addEntity(new AbstractEntity(this, 0, 6, this.width, 1, false, this.app.platform.colorByName('brightBlack')));
     this.addEntity(new AbstractEntity(this, 0, 6, 1, this.height-6, false, this.app.platform.colorByName('brightBlack')));
     this.addEntity(new AbstractEntity(this, 0, this.height-1, this.width, 1, false, this.app.platform.colorByName('brightBlack')));
     this.addEntity(new AbstractEntity(this, this.width-1, 6, 1, this.height-6, false, this.app.platform.colorByName('brightBlack')));
-    this.addEntity(new MiniTextEntity(this, 0, 0, 64, 7, 'HALL OF FAME', this.app.platform.colorByName('brightWhite'), this.app.platform.colorByName('brightBlack'), 1, 1));
+    var titleEntity = new MiniTextEntity(this, 0, 0, this.width, 9, 'HALL OF FAME', this.app.platform.colorByName('brightWhite'), this.app.platform.colorByName('brightBlack'), 1, 2);
+    titleEntity.justify = 2;
+    this.addEntity(titleEntity);
+    this.addEntity(new MiniButtonEntity(this, this.width-38, this.height-15, 36, 13, 'CLOSE', 'closeAbout', ['Enter', 'Escape', ' '], this.app.platform.colorByName('brightWhite'), this.app.platform.colorByName('brightBlue'), 1, 4));
   } // init
 
   handleEvent(event) {
