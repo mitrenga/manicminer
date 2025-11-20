b4ea579615a353a68a48eccd2934a80f071bf33b
Libor Mitrenga <libor@mitrengovi.cz>
2025-11-20 11:26:50 +0100

    Kosmetická úprava v zachytávání eventů

--- app/main.js
+++ app/main.js
@@ -16,24 +16,24 @@ function loopGame(timestamp) {
 } // loopGame
 
 // events processing
-window.onkeydown = function(event) { gameApp.inputEventsManager.eventKeyDown(event); }
-window.onkeyup = function(event) { gameApp.inputEventsManager.eventKeyUp(event); }
-window.onclick = function(event) { gameApp.inputEventsManager.eventClick(event); }
-window.oncontextmenu = function(event) { gameApp.inputEventsManager.eventContextMenu(event); }
-window.onmousedown = function(event) { gameApp.inputEventsManager.eventMouseDown(event); }
-window.onmouseup = function(event) { gameApp.inputEventsManager.eventMouseUp(event); }
-window.onmousemove = function(event) { gameApp.inputEventsManager.eventMouseMove(event); }
-window.ontouchstart = function(event) {gameApp.inputEventsManager.eventTouchStart(event); }
-window.ontouchend = function(event) {gameApp.inputEventsManager.eventTouchEnd(event); }
-window.ontouchcancel = function(event) {gameApp.inputEventsManager.eventTouchEnd(event); }
-window.ontouchmove = function(event) {gameApp.inputEventsManager.eventTouchMove(event); }
-window.onblur = function(event) { gameApp.inputEventsManager.eventBlurWindow(event); }
-window.onfocus = function(event) { gameApp.inputEventsManager.eventFocusWindow(event); }
-window.onresize = function(event) { gameApp.eventResizeWindow(event); }
+window.addEventListener('keydown', (event) => gameApp.inputEventsManager.eventKeyDown(event));
+window.addEventListener('keyup', (event) => gameApp.inputEventsManager.eventKeyUp(event));
+window.addEventListener('click', (event) => gameApp.inputEventsManager.eventClick(event));
+window.addEventListener('contextmenu', (event) => gameApp.inputEventsManager.eventContextMenu(event));
+window.addEventListener('mousedown', (event) => gameApp.inputEventsManager.eventMouseDown(event));
+window.addEventListener('mouseup', (event) => gameApp.inputEventsManager.eventMouseUp(event));
+window.addEventListener('mousemove', (event) => gameApp.inputEventsManager.eventMouseMove(event));
+window.addEventListener('touchstart', (event) => gameApp.inputEventsManager.eventTouchStart(event));
+window.addEventListener('touchend', (event) => gameApp.inputEventsManager.eventTouchEnd(event));
+window.addEventListener('touchcancel', (event) => gameApp.inputEventsManager.eventTouchEnd(event));
+window.addEventListener('touchmove', (event) => gameApp.inputEventsManager.eventTouchMove(event));
+window.addEventListener('blur', (event) => gameApp.inputEventsManager.eventBlurWindow(event));
+window.addEventListener('focus', (event) => gameApp.inputEventsManager.eventFocusWindow(event));
+window.addEventListener('resize', (event) => gameApp.eventResizeWindow(event));
 
 if ('GamepadEvent' in window) {
-  window.ongamepadconnected = function(event) { gameApp.inputEventsManager.eventGamepadConnected(event); }
-  window.ongamepaddisconnected = function(event) { gameApp.inputEventsManager.eventGamepadDisconnected(event); }
+  window.addEventListener('gamepadconnected', (event) => gameApp.inputEventsManager.eventGamepadConnected(event));
+  window.addEventListener('gamepaddisconnected', (event) => gameApp.inputEventsManager.eventGamepadDisconnected(event));
   gameApp.controls.gamepads.supported = true;
 }
 
