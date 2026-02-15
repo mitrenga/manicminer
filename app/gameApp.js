2088eee332d5c0ba5ba4df5e125b4f1eedfeb849
Libor Mitrenga <libor@mitrengovi.cz>
2026-02-15 22:23:20 +0100

    Číslo verze 2026.02.15

--- app/gameApp.js
+++ app/gameApp.js
@@ -34,7 +34,7 @@ export class GameApp extends AbstractApp {
   constructor(platform, importPath, wsURL) {
     super(platform, 'bodyApp', importPath, wsURL);
 
-    this.version = '2026.02.13';
+    this.version = '2026.02.15';
     this.copyright = '© 2025 GNU General Public Licence';
     
     this.audioManager = new AudioManager(this);
